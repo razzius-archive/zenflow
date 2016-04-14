294975e671335353c660ffdda9a970579108c1c2
Razzi Abuissa <razzi53@gmail.com>
2016-04-14 14:19:27 -0700

    Refactor start.py to use lib.py

---
+++ lib.py
@@ -0,0 +1,113 @@
+import logging
+import os
+import asyncio
+
+import aiohttp
+import requests
+
+
+logger = logging.getLogger()
+# logger.setLevel('DEBUG')
+
+
+PIPELINE_IDS = {
+    'backlog': '54c19ad8f748cd180f07b4b6',
+    'in_development': '54c19ad8f748cd180f07b4b4',
+    'new_issues': '54c19ad8f748cd180f07b4b7',
+    'pr_outstanding': '55c2c7964e6d61ea173a1ce8'
+}
+
+MILESTONE_IDS = {
+    'v1.17': 85,
+    'v1.18': 88
+}
+
+
+CLINT_REPO_ID = '14154595'
+
+
+async def _get_zenhub_issues(client):
+    async with client.get('https://api.zenhub.io/p1/repositories/14154595/board', headers={
+        'X-Authentication-Token': os.environ['ZENHUB_API_TOKEN']
+    }) as response:
+        return await response.json()
+
+
+async def _get_github_issues(client, assignee, milestone_id):
+    async with client.get(
+        'https://api.github.com/repos/sighten/clint/issues',
+        headers={
+            'Authorization': 'Token {}'.format(os.environ['GITHUB_API_TOKEN'])
+        },
+        params={
+            'assignee': assignee,
+            'milestone': milestone_id
+        }
+    ) as response:
+        return await response.json()
+
+
+async def _zenhub_issue_numbers(loop, pipeline_name):
+    """Returns a list of issue numbers ordered by ZenHub board position (indicates priority)."""
+    with aiohttp.ClientSession(loop=loop) as client:
+        content = await _get_zenhub_issues(client)
+
+    in_development = next(
+        p for p in content['pipelines']
+        if p['name'].lower().replace(' ', '_').startswith(pipeline_name)
+    )
+
+    return [issue['issue_number'] for issue in in_development['issues']]
+
+
+async def _github_issue_numbers(loop, assignee, milestone_id):
+    with aiohttp.ClientSession(loop=loop) as client:
+        content = await _get_github_issues(client, assignee, milestone_id)
+
+    return {
+        issue['number'] for issue in content
+    }
+
+
+async def get_top_issue_by_pipeline(loop, pipeline_name, username, milestone_number):
+    zenhub_priorities = await _zenhub_issue_numbers(loop, pipeline_name)
+    github_issues = await _github_issue_numbers(loop, username, MILESTONE_IDS[milestone_number])
+
+    logger.debug('Zenhub priorities:')
+    logger.debug(zenhub_priorities)
+
+    logger.debug('Github issues:')
+    logger.debug(github_issues)
+
+    return next(
+        issue for issue in zenhub_priorities
+        if issue in github_issues
+    )
+
+
+async def change_issue_pipeline(loop, issue_number, from_pipeline_id, to_pipeline_id):
+    # todo do I need to pass the from_pipeline_id
+    # TODO do I need to pass the issue ID in the url and body?
+
+    api_url = 'https://api.zenhub.io/v3/repositories/{}/issues/{}/pipelines'.format(
+        CLINT_REPO_ID, issue_number)
+
+    with aiohttp.ClientSession(loop=loop) as client:
+        return await client.put(
+        api_url,
+        headers={
+            'x-authentication-token': os.environ['ZENHUB_INTERNAL_API_TOKEN']
+        },
+        data={
+            'issue_number': issue_number,
+            'repo_id': CLINT_REPO_ID,
+            'from_pipeline_id': PIPELINE_IDS['backlog'],
+            'pipeline_id': PIPELINE_IDS['in_development']
+        }
+    )
+
+
+def async_execute(main_function, *main_args, **main_kwargs):
+    """Run an asynchronous main function."""
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main_function(loop, *main_args, **main_kwargs))
--- start.py
+++ start.py
@@ -1,111 +1,25 @@
 #!/usr/bin/env python3.5
 
-# TODO this is copypasta
-
-import aiohttp
-import argparse
-import asyncio
-import os
-
-import requests
-
-BACKLOG_PIPELINE_ID = '54c19ad8f748cd180f07b4b6'
-IN_DEVELOPMENT_PIPELINE_ID = '54c19ad8f748cd180f07b4b4'
+from lib import (
+    PIPELINE_IDS,
+    async_execute,
+    get_top_issue_by_pipeline,
+    change_issue_pipeline
+)
 
 
-async def get_zenhub_issues(client):
-    async with client.get('https://api.zenhub.io/p1/repositories/14154595/board', headers={
-        'X-Authentication-Token': os.environ['ZENHUB_API_TOKEN']
-    }) as response:
-        return await response.json()
+async def main(loop):
+    issue_number = await get_top_issue_by_pipeline(loop, 'backlog', 'razzius', 'v1.17')
 
+    print('Moving issue {} to "In Development"'.format(issue_number))
 
-async def get_github_issues(client):
-    async with client.get(
-        'https://api.github.com/repos/sighten/clint/issues',
-        headers={
-            'Authorization': 'Token {}'.format(os.environ['GITHUB_API_TOKEN'])
-        },
-        params={
-            'milestone': 82,
-            'assignee': 'razzius'
-        }
-    ) as response:
-        return await response.json()
+    response = await change_issue_pipeline(loop,
+                                           issue_number,
+                                           PIPELINE_IDS['backlog'],
+                                           PIPELINE_IDS['in_development'])
 
-
-def parse_zenhub_issue_numbers(content):
-    """Returns a list of issue numbers ordered by ZenHub board position (indicates priority)."""
-    in_development = next(
-        p for p in content['pipelines']
-        if p['name'].startswith('Backlog')
-    )
-
-    return [x['issue_number'] for x in in_development['issues']]
-
-
-def parse_github_issue_numbers(content):
-    return {issue['number'] for issue in content}
-
-
-async def get_issue_numbers(loop):
-    with aiohttp.ClientSession(loop=loop) as client:
-        content = await get_zenhub_issues(client)
-        issue_numbers = parse_zenhub_issue_numbers(content)
-        return issue_numbers
-
-
-async def get_my_issues(loop):
-    with aiohttp.ClientSession(loop=loop) as client:
-        content = await get_github_issues(client)
-        issue_numbers = parse_github_issue_numbers(content)
-        return issue_numbers
-
-
-async def main(verbosity=0):
-    zenhub_priorities = await get_issue_numbers(loop)
-    github_issues = await get_my_issues(loop)
-
-    if verbosity:
-        print('Zenhub priorities:')
-        print(zenhub_priorities)
-
-        print('Github issues:')
-        print(github_issues)
-
-    target = next(
-        issue for issue in zenhub_priorities
-        if issue in github_issues
-    )
-
-    print('Moving issue {} to "In Development"'
-          .format(target))
-
-    # TODO do I need to pass the issue ID in the url and body?
-    api_url = 'https://api.zenhub.io/v3/repositories/14154595/issues/{}/pipelines'.format(target)
-
-    resp = requests.put(
-        api_url,
-        headers={
-            'x-authentication-token': os.environ['ZENHUB_INTERNAL_API_TOKEN']
-        },
-        data={
-            'issue_number': target,
-            'repo_id': 14154595,
-            'from_pipeline_id': BACKLOG_PIPELINE_ID,
-            'pipeline_id': IN_DEVELOPMENT_PIPELINE_ID
-        }
-    )
-
-    print(resp)
+    # todo parse the output
+    print(response)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbosity',
-                        help='print debug information',
-                        default=0,
-                        action='count')
-    args = parser.parse_args()
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(args.verbosity))
+    async_execute(main)
